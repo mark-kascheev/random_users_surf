b2a6c56e1e9544a5af6d6c6092eac2151ebf773e
mark-kascheev <mark.kascheev@gmail.com>
2022-07-06 18:23:01 +0300

    Register dependencies for wm

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:random_users_surf/app_model.dart';
+import 'package:random_users_surf/data/service/auth_service_impl.dart';
+import 'package:random_users_surf/domain/service/auth_service.dart';
+import 'package:random_users_surf/screen/login_screen/login_screen.dart';
 
 void main() {
+  final getIt = GetIt.instance;
+  getIt.registerSingleton<AppModel>(AppModel());
+  getIt.registerSingleton<AuthService>(AuthServiceImpl());
   runApp(const MyApp());
 }
 
@@ -9,6 +17,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const MaterialApp(home: LoginScreen());
   }
-  }
+}
