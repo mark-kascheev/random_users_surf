2cd0e9af1e12de496b62ef686d00fb8c2f00c362
mark-kascheev <mark.kascheev@gmail.com>
2022-07-06 18:22:00 +0300

    Add validation of user login

--- lib/screen/login_screen/login_widget_model.dart
+++ lib/screen/login_screen/login_widget_model.dart
@@ -1,29 +1,64 @@
 import 'package:elementary/elementary.dart';
 import 'package:flutter/material.dart';
 import 'package:random_users_surf/app_model.dart';
+import 'package:random_users_surf/domain/service/auth_service.dart';
 import 'package:random_users_surf/screen/login_screen/login_screen.dart';
 import 'package:random_users_surf/screen/login_screen/login_screen_model.dart';
 import 'package:random_users_surf/screen/users_screen/users_screen.dart';
+import 'package:get_it/get_it.dart';
 
 class LoginWidgetModel extends WidgetModel<LoginScreen, LoginScreenModel> {
-  TextEditingController loginFieldController = TextEditingController();
   final AppModel _appModel;
-  LoginWidgetModel(super.model, {required AppModel appModel}) : _appModel = appModel;
+  final TextEditingController loginFieldController = TextEditingController();
+  final ValueNotifier<bool> buttonEnabled = ValueNotifier<bool>(false);
+
+  LoginWidgetModel(super.model, {required AppModel appModel})
+      : _appModel = appModel;
 
   @override
   void initWidgetModel() {
     super.initWidgetModel();
 
     _appModel.addListener(onAuthUser);
+    loginFieldController.addListener(onTextChanged);
+  }
+
+  void onTextChanged() {
+    if (_isInputLoginValid()) {
+      buttonEnabled.value = true;
+    } else {
+      buttonEnabled.value = false;
+    }
   }
 
   void onLoginPressed() {
-    model.loginUser(loginFieldController.text);
+    if (_isInputLoginValid()) {
+      buttonEnabled.value = true;
+      model.loginUser(loginFieldController.text);
+    }
   }
 
   void onAuthUser() {
-    if(_appModel.user != null) {
-      Navigator.of(context).push(MaterialPageRoute(builder: (_) => const UsersScreen()));
+    buttonEnabled.value = false;
+    if (_appModel.user != null) {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (_) => const UsersScreen()));
     }
   }
- }
+
+  bool _isInputLoginValid() {
+    final inputLogin = loginFieldController.text.trim();
+    if (inputLogin.isEmpty) {
+      return false;
+    }
+    return true;
+  }
+}
+
+LoginWidgetModel createLoginWM(BuildContext _) {
+  final getIt = GetIt.instance;
+  final appModel = getIt<AppModel>();
+  return LoginWidgetModel(
+      appModel: appModel,
+      LoginScreenModel(appModel: appModel, authService: getIt<AuthService>()));
+}
